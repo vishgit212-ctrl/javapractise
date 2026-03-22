342e42e5162d20ffadb65256494442f01622aee3
Vishgit212 <vishgit212@gmail.com>
2026-03-22 14:55:05 +0530

    datatype

--- datatype.java
+++ datatype.java
@@ -3,9 +3,53 @@ public class datatype {
             // variable declaration and initialization
             // primitives data types
             // whole numbers
-              // byte, short, int, long
+              // byte, 
+              //  short,
+              //  int,
+              //  long
+
+
+                int age = 25;
+                // byte age1 = 20 ;
+                //  short  age3 = 20 ;
+                //   long  age4 = 204387647373882733l ; // hame batana padega ki ye long hai warna compiler ise int samjhega
+                System.out.println("Age: " + age);
+
+            // System.out.println(Byte.MIN_VALUE);
+            // System.out.println(Byte.MAX_VALUE);
+
+            // System.out.println(Short.MIN_VALUE);
+            // System.out.println(Short.MAX_VALUE);
+
+            // System.out.println(Integer.MIN_VALUE);
+            // System.out.println(Integer.MAX_VALUE);
+
+
+            System.out.println(Long.MIN_VALUE);
+            System.out.println(Long .MAX_VALUE);
+
+
+
             // decimal numbers
               // float, double
+
+
+            float mysalry = 19.92339f; // hame batana padega ki ye float hai warna compiler ise double samjhega
+            double yoursalry = 3.14159;
+            System.out.println( mysalry);
+            System.out.println(yoursalry);
+
+
+
+
+
+
+
+
+
+
+
+
             //characters
               // char
             // boolean
