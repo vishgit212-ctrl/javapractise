339ec2721b1e1462f402d650c8853a1f2e97699f
Vishgit212 <vishgit212@gmail.com>
2026-03-22 13:00:06 +0530

    java practice files

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.print("Hello World"); // ya line break karta ha  iska bad jo bhi print hoga wo next line me print hoga
+    System.out.print("This is a Java program.");//  ya line break nahi karta ha 
+
+
+
+    
+
+  }
+
+  
+}
---
+++ datatype.java
@@ -0,0 +1,15 @@
+public class datatype {
+        public static void main(String[] args) {
+            // variable declaration and initialization
+            // primitives data types
+            // whole numbers
+              // byte, short, int, long
+            // decimal numbers
+              // float, double
+            //characters
+              // char
+            // boolean
+              // boolean
+
+        }
+}
