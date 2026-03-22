aa581c138892e686da9881a01831c62e94a92985
Vishgit212 <vishgit212@gmail.com>
2026-03-22 20:22:34 +0530

    datatype

--- datatype.java
+++ datatype.java
@@ -9,11 +9,11 @@ public class datatype {
               //  long
 
 
-                int age = 25;
+               // int age = 25;
                 // byte age1 = 20 ;
                 //  short  age3 = 20 ;
                 //   long  age4 = 204387647373882733l ; // hame batana padega ki ye long hai warna compiler ise int samjhega
-                System.out.println("Age: " + age);
+               // System.out.println("Age: " + age);
 
             // System.out.println(Byte.MIN_VALUE);
             // System.out.println(Byte.MAX_VALUE);
@@ -25,8 +25,8 @@ public class datatype {
             // System.out.println(Integer.MAX_VALUE);
 
 
-            System.out.println(Long.MIN_VALUE);
-            System.out.println(Long .MAX_VALUE);
+            // System.out.println(Long.MIN_VALUE);
+            // System.out.println(Long .MAX_VALUE);
 
 
 
@@ -34,25 +34,35 @@ public class datatype {
               // float, double
 
 
-            float mysalry = 19.92339f; // hame batana padega ki ye float hai warna compiler ise double samjhega
-            double yoursalry = 3.14159;
-            System.out.println( mysalry);
-            System.out.println(yoursalry);
-
-
-
-
-
-
-
-
-
-
+            // float mysalry = 19.92339f; // hame batana padega ki ye float hai warna compiler ise double samjhega
+            // double yoursalry = 3.14159;
+            // System.out.println( mysalry);
+            // System.out.println(yoursalry);
 
 
             //characters
               // char
-            // boolean
+           // char grade = 'A';// char ma sirf ek character store kar sakta hai aur usko single quote ma likhna hota hai
+                //System.out.println(grade);
+
+           // String name = "John Doe"; // String is not a primitive data type but it is a class in java
+               // System.out.println(name);
+
+
+              //  char initial = 'z';
+            //    System.out.println( (int) initial);// isko int ma convert karne par hame uska ascii value mil jayega
+
+
+            char heartsymbol = 'z';
+                System.out.println( (int) heartsymbol);// isko int ma convert karne par hame uska ascii value mil jayega
+
+            char hindichar = 2300; // unicode character for hindi character
+                System.out.println(hindichar); // isko char ma convert karne par hame uska unicode character mil jayega
+
+            
+
+
+              // boolean
               // boolean
 
         }
